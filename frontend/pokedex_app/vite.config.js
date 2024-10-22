1ef65cb89a9c276942905d53e942d41904bc17b7
Leonor Tu <Jingyi.Tu@outlook.com>
2024-10-22 12:57:47 +0300

    Feat: add backend url from render

--- frontend/pokedex_app/vite.config.js
+++ frontend/pokedex_app/vite.config.js
@@ -13,9 +13,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://webdev-express.onrender.com',
         changeOrigin: true,
-        secure: false,
       },
     },
   },
